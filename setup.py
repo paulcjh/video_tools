a92d618b4079c8b2383fb42b66085d89744cc9b3
Paul Hetherington <paul@neuro-ai.co.uk>
2021-06-19 14:13:52 +0100

    installation scripts

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="video_toos",
+    version="0.0.1",
+    author="paulcjh",
+    author_email="pcj.hetherington@gmail.com",
+    description="Some groovy video tools focused on ML.",
+    long_description=long_description,
+    url="https://github.com/paulcjh/video_tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
